b0f620473857a87db2e6f8eb6e930085cc082372
Tong Wu <eric.w@wustl.edu>
2021-11-01 16:36:08 -0500

    change for the testing

--- siroutput_full.m
+++ siroutput_full.m
@@ -11,17 +11,17 @@ function f = siroutput_full(x,t)
 k_infections = x(1);
 k_fatality = x(2);
 k_recover = x(3);
-k_recover_toS = x(4);
+% k_recover_toS = x(4);
 
 % set up initial conditions
-ic_susc = x(5);
-ic_inf = x(6);
-ic_rec = x(7);
-ic_fatality = x(8);
+ic_susc = x(4);
+ic_inf = x(5);
+ic_rec = x(6);
+ic_fatality = x(7);
 
 % Set up SIRD within-population transmission matrix
-A = [(1-k_infections)   k_recover_toS           0 0; 
-        k_infections        (1-k_recover_toS-k_recover-k_fatality)           0 0;
+A = [(1-k_infections)   0          0 0; 
+        k_infections        (1-k_recover-k_fatality)           0 0;
         0                   k_recover   1 0;
         0                   k_fatality   0 1];
 
