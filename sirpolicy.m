b25255c6a2549fd236550d773ec88db98ed5c514
Tong Wu <eric.w@wustl.edu>
2021-11-08 21:10:34 -0600

    graphs

--- sirpolicy.m
+++ sirpolicy.m
@@ -2,7 +2,8 @@ function [policy_matrix] = sirpolicy(current_policy, slird_vals)
 
 % this function returns a new policy (for the next time step) based on the current policy and current SLIRD values
 % slird_vals: a 5 dimensional vector containing the current proportion of individuals in susceptible, lockdown, infected, recovered and deceased
-% current_polc: a 5x5 matrix containing the current SLIRD policy (i.e., the state transition matrix)
+% current_polc: a 5x5 matrix containing the current SLIRD policy 
+
 policy_matrix = current_policy; % Set it to the status quo
 % If the infection population is low and lockdown population is high and
 % rate of remaining in the lockdown is high, we move people outside of
